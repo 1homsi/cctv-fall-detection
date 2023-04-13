33d5af20be137114d300779696762682f92078aa
SaraBaassiri <sarabsr95@gmail.com>
2023-04-13 13:28:26 +0300

    fixes

--- fyp_website/src/screens/products/products.jsx
+++ fyp_website/src/screens/products/products.jsx
@@ -70,17 +70,17 @@ onClick={() => {
       <div className="productsInner">
         <div className="itemCards">
           <h1>1 month </h1>
-          <h3>$100</h3>
+          <h3>$99.99</h3>
           <button>Get Now!</button>
         </div>
         <div className="itemCards">
           <h1>3 months</h1>
-          <h3>$250</h3>
+          <h3>$249.99</h3>
           <button>Get Now!</button>
         </div>
         <div className="itemCards">
           <h1>1 year</h1>
-          <h3>$1000</h3>
+          <h3>$1,099.99</h3>
           <button>Get Now!</button>
         </div>
       </div>
@@ -89,51 +89,18 @@ onClick={() => {
           
       <div className="productsInner">
       <div className="itemCards">
-        <h1>temp </h1>
-        <p>
-          Imagine having an AI that watches over you, with every step you
-          take, every move you make. This AI is always on guard, scanning your
-          movements for signs of imbalance or instability. It's your guardian
-          angel, ready to leap into action at a moment's notice if you start
-          to fall. With advanced sensors and algorithms, this AI can deploy
-          safety features, call for emergency assistance, and even analyze
-          your movements to identify areas where you may be at risk of
-          falling. It's like having a superhero by your side, always watching
-          over you and protecting you from harm.
-        </p>
-        <h3>{monthly ? "100$ / month" : "1150$ / year"}</h3>
+        <h1>1 month</h1>
+        <h3>$89.99</h3>
         <button>Get Now!</button>
       </div>
       <div className="itemCards">
-        <h1>Face Recognition</h1>
-        <p>
-          Imagine having an AI that can read people's emotions with remarkable
-          accuracy, like a mind-reading machine. This AI is designed to detect
-          and analyze the emotional state of customers, allowing businesses to
-          measure their satisfaction levels and respond proactively to their
-          needs. With its advanced algorithms and machine learning
-          capabilities, this emotion detection AI can pick up on subtle cues
-          like facial expressions, tone of voice, and body language, providing
-          real-time feedback to help businesses improve their customer
-          service. It's like having a secret weapon for customer satisfaction,
-          giving businesses the power to connect with their customers on a
-          deeper level and build long-lasting relationships.
-        </p>
-        <h3>{monthly ? "100$ / month" : "1150$ / year"}</h3>
+        <h1>3 months</h1>
+        <h3>$179.99</h3>
         <button>Get Now!</button>
       </div>
       <div className="itemCards">
-        <h1>The Best</h1>
-        <p>
-          This advanced AI not only protects you from falls but also detects
-          your emotional state to gauge your satisfaction as a customer. It
-          can pick up on subtle cues like facial expressions, tone of voice,
-          and body language, providing businesses with real-time feedback to
-          help them improve customer service. It's like having a personal
-          guardian angel and a secret weapon for customer satisfaction rolled
-          into one.
-        </p>
-        <h3>{monthly ? "150$ / month" : "1800$ / year"}</h3>
+        <h1>1 year</h1>
+        <h3>$1,079.99</h3>
         <button>Get Now!</button>
       </div>
     </div>
@@ -141,51 +108,18 @@ onClick={() => {
           
       <div className="productsInner">
       <div className="itemCards">
-        <h1>fuck </h1>
-        <p>
-          Imagine having an AI that watches over you, with every step you
-          take, every move you make. This AI is always on guard, scanning your
-          movements for signs of imbalance or instability. It's your guardian
-          angel, ready to leap into action at a moment's notice if you start
-          to fall. With advanced sensors and algorithms, this AI can deploy
-          safety features, call for emergency assistance, and even analyze
-          your movements to identify areas where you may be at risk of
-          falling. It's like having a superhero by your side, always watching
-          over you and protecting you from harm.
-        </p>
-        <h3>{monthly ? "100$ / month" : "1150$ / year"}</h3>
+        <h1>1 month</h1>
+        <h3>$199.99</h3>
         <button>Get Now!</button>
       </div>
       <div className="itemCards">
-        <h1>Face Recognition</h1>
-        <p>
-          Imagine having an AI that can read people's emotions with remarkable
-          accuracy, like a mind-reading machine. This AI is designed to detect
-          and analyze the emotional state of customers, allowing businesses to
-          measure their satisfaction levels and respond proactively to their
-          needs. With its advanced algorithms and machine learning
-          capabilities, this emotion detection AI can pick up on subtle cues
-          like facial expressions, tone of voice, and body language, providing
-          real-time feedback to help businesses improve their customer
-          service. It's like having a secret weapon for customer satisfaction,
-          giving businesses the power to connect with their customers on a
-          deeper level and build long-lasting relationships.
-        </p>
-        <h3>{monthly ? "100$ / month" : "1150$ / year"}</h3>
+        <h1>3 months</h1>
+        <h3>$399.99</h3>
         <button>Get Now!</button>
       </div>
       <div className="itemCards">
-        <h1>Fuck Fukc</h1>
-        <p>
-          This advanced AI not only protects you from falls but also detects
-          your emotional state to gauge your satisfaction as a customer. It
-          can pick up on subtle cues like facial expressions, tone of voice,
-          and body language, providing businesses with real-time feedback to
-          help them improve customer service. It's like having a personal
-          guardian angel and a secret weapon for customer satisfaction rolled
-          into one.
-        </p>
-        <h3>{monthly ? "150$ / month" : "1800$ / year"}</h3>
+        <h1>1 year</h1>
+        <h3>$2,399.99</h3>
         <button>Get Now!</button>
       </div>
     </div>
